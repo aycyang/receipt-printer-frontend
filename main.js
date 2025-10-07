0d6c67a2630a7fe6e234b6c90bd0bb7e8a3944eb
Julia Tufts <juliatufts@gmail.com>
2025-10-06 23:05:31 -0400

    fix image upload, add copy image hex button, finesse styling

--- main.js
+++ main.js
@@ -105,98 +105,95 @@ function updateImagePreview(imageInput, preview) {
     preview.appendChild(para);
   } else {
     const file = files[0];
-    const imageItem = document.createElement("div");
-    preview.appendChild(imageItem);
-
+    const imageContainer = document.createElement("div");
     const para = document.createElement("p");
+    preview.appendChild(imageContainer);
+
     if (validFileType(file)) {
-      para.textContent = `File name ${file.name}, file size ${returnFileSize(
-        file.size
-      )}.`;
+      para.textContent = `File size: ${returnFileSize(file.size)}.`;
       const image = document.createElement("img");
       image.src = URL.createObjectURL(file);
       image.alt = image.title = file.name;
 
-      imageItem.appendChild(image);
-      imageItem.appendChild(para);
+      imageContainer.appendChild(image);
+      imageContainer.appendChild(para);
+
+      image.addEventListener("load", (event) => {
+        // image is fully loaded, can now pull data intrinsic to img tag
+        console.log(event.target);
+        console.log(event.target.width);
+        console.log(event.target.height);
+        var data = getImageData(event.target);
+        var hex = convertImageDataToHex(data);
+        refreshImageHex(hex);
+      });
     } else {
       para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
-      imageItem.appendChild(para);
+      imageContainer.appendChild(para);
     }
   }
 }
 
-// TODO: get this working
-function convertImage(imageFile) {
-  var img = document.createElement("img");
+// image is a reference to an img element
+function getImageData(image) {
   var canvas = document.createElement("canvas");
   var ctx = canvas.getContext("2d");
-  var data = "";
-
-  const file = imageFile.files[0];
-  console.log(imageFile.files);
-  img.src = URL.createObjectURL(file);
-  console.log(img.height);
-
-  canvas.height = img.height;
-  canvas.width = img.width;
-  ctx.drawImage(img, 0, 0);
-  data = ctx.getImageData(0, 0, img.width, img.height);
-  console.log("IMG DATA");
-  console.log(data);
+  canvas.height = image.height;
+  canvas.width = image.width;
+  ctx.drawImage(image, 0, 0);
+  return ctx.getImageData(0, 0, image.width, image.height);
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-  const textInput = document.getElementById("textInput");
-  const imageInput = document.getElementById("imageInput");
-  const imagePreview = document.getElementById("imagePreview");
-
-  // For now, grab image rendered directly in HTML and use that data
-  const actualImg = document.getElementById("actualImg");
-  console.log(actualImg);
-
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-  var width = actualImg.width;
-  var height = actualImg.height;
-
-  canvas.height = height;
-  canvas.width = width;
-  ctx.drawImage(actualImg, 0, 0);
-  var data = ctx.getImageData(0, 0, actualImg.width, actualImg.height).data;
-  console.log("IMG DATA");
-  console.log(data);
-
+function convertImageDataToHex(imageData) {
+  var data = imageData.data;
+  var width = imageData.width;
+  var height = imageData.height;
   var bits = [];
   var paddedWidth = Math.floor((width + 7) / 8) * 8;
+
   for (var y = 0; y < height; y++) {
     for (var x = 0; x < paddedWidth; x++) {
       if (x >= width) {
         bits[bits.length - 1] = bits[bits.length - 1] << 1;
         continue;
       }
-      var r = data[y * width * 4 + x * 4];
+      var r = data[y * width * 4 + x * 4]; // only use red value for now
       if (x % 8 === 0) {
         bits.push(0);
       } else {
         bits[bits.length - 1] = bits[bits.length - 1] << 1;
       }
 
+      // hard split into black/white for now
       if (r < 127) {
         bits[bits.length - 1] = bits[bits.length - 1] | 1;
       }
     }
   }
   const bitArray = new Uint8Array(bits);
-  console.log(bitArray);
   console.log("length: " + bitArray.length);
-  console.log(bitArray.toHex());
-  // End bespoke code zone
+  return bitArray.toHex();
+}
 
-  // TODO: get this working
+function refreshImageHex(hex) {
+  const imageHex = document.getElementById("imageHex");
+  imageHex.innerHTML = hex;
+  // TODO: bake in command containing width/height
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const textInput = document.getElementById("textInput");
+  const imageInput = document.getElementById("imageInput");
+  const imagePreview = document.getElementById("imagePreview");
+  const copyBtn = document.getElementById("copyImageHexBtn");
+
   imageInput.addEventListener("change", () => {
     updateImagePreview(imageInput, imagePreview);
-    convertImage(imageInput);
+  });
+
+  copyBtn.addEventListener("click", (e) => {
+    const imageHexText = document.getElementById("imageHex");
+    navigator.clipboard.writeText(imageHexText.innerHTML);
   });
 
   document.getElementById("form").addEventListener("submit", (e) => {
